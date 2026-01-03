ca42bd68f4983db3835ead56cc6c574e0952e5ed
keerti0singh-cmyn <keerti0singh@gmail.com>
2026-01-03 10:09:40 +0530

    Allow Cloudinary images in Next Image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['cloudinary'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
 }
 
 export default nextConfig
